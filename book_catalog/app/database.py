e7fc3c082c7f0a0b4c612ce44dc9645c0e61b74e
Manasa-44 <manasa.kavali2001@gmail.com>
2024-09-20 23:58:15 +1000

    commit

--- book_catalog/app/database.py
+++ book_catalog/app/database.py
@@ -3,8 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://db_722task7p2_user:i6IXzaM7YCtr6UC8GXhLIVBvfkWuSnxC@dpg-crhvjsjtq21c738477ag-a.oregon-postgres.render.com/db_722task7p2" #os.getenv("DATABASE_URL")
-
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
